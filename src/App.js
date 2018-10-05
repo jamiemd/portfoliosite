73ff39c77e3c4b0ac963721a2d0a9dad34df2ce7
Jamie <jamiedomingo@hotmail.com>
2018-10-04 19:37:59 -0700

    added favicon and title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Home from "./components/home";
-import './App.css';
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Home/>
+        <Home />
       </div>
     );
   }
